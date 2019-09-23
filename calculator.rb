71897893723860ceae4f185ab90c2369cf2ce4db
Jay  Stewart <jayr.stewart@live.com>
2019-09-23 17:32:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,7 @@
-first_number = 1
+first_number = 6
 second_number = 12
+
 sum = first_number + second_number
+difference = second_number - first_number
+product = first_number*second_number
+quotient = second_number / first_number
